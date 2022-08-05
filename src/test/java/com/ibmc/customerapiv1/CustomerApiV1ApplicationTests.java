468ebbf486c1165e4904eac25e0f907c84f815d4
BinduSK <bindusajithkumar01@gmail.com>
2022-08-06 05:22:32 +0530

    first commit

---
+++ src/test/java/com/ibmc/customerapiv1/CustomerApiV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmc.customerapiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApiV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
